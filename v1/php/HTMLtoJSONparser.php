6668767dc3d6d6a98fd051e8e079782b14c25aa9
Luca Kiebel <contact@luca-kiebel.de>
2017-01-14 14:34:26 +0100

    Add Version 1

---
+++ v1/php/HTMLtoJSONparser.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luckie
+ * Date: 12.01.17
+ * Time: 17:13
+ */
+
+class HBBK_API
+{
+    public static function authenticate(String $username, String $password){
+
+    }
+}
